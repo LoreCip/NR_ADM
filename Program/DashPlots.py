Tests and fixes for the derivatives

Program/DashPlots.py
@@ -19,7 +19,7 @@ def maps(string_val):
 		'A' : 0, 'B' : 1,
 		'DA': 2, 'DB': 3,
 		'KA': 4, 'KB': 5,
-		'alpha': 6
+		'alpha': 6, 'Dalpha':7
 	}
 	return mapps[string_val]
 
@@ -27,12 +27,12 @@ def maps(string_val):
 def setup(name, N, itmax, r, dR):
     ## RECOVER DATA
     dataH5 = h5py.File(name,'r')
-    data = np.zeros((len(dataH5), 7, N-2))
-    for i in range(1,itmax+1):
+    data = np.zeros((len(dataH5), 8, N-2))
+    for i in range(1,itmax):
         data[i,:,:] = dataH5[f'{i}']
         
     horizonData = dataH5['Horizon']
-
+    
     ## SETUP DASH    
     app = dash.Dash()   #initialising dash app
     
@@ -44,8 +44,8 @@ def setup(name, N, itmax, r, dR):
          idG = maps(dropdown_value)
          idT = slider_value
          
-         fig = go.Figure([go.Scatter(x = r, y = data[idT,idG,:], line = dict(color = 'firebrick', width = 4), name = dropdown_value)])
-         fig.update_layout(title = f'Time {idT}', xaxis_title = 'Position')
+         fig = go.Figure([go.Scatter(x = r[r<3], y = data[idT,idG,:][r<3], line = dict(color = 'firebrick', width = 4), name = dropdown_value)])
+         fig.update_layout(title = f'Time {idT}', xaxis_title = 'Position', )
          return fig  
     
     def horizon_plot():
@@ -68,8 +68,7 @@ def setup(name, N, itmax, r, dR):
             )
     
         fig.update_layout(title = f'Horizon', xaxis_title = 'Time')
-        return fig  
-    
+        return fig
     
     ##   PAGE LAYOUT
     children = [
@@ -86,7 +85,8 @@ def setup(name, N, itmax, r, dR):
                            {'label': 'DB', 'value':'DB'},
                            {'label': 'KA', 'value':'KA'},
                            {'label': 'KB', 'value':'KB'},
-                           {'label': r'\alpha', 'value':'alpha'}
+                           {'label': 'al', 'value':'alpha'},
+                           {'label': 'Dal','value':'Dalpha'}
                            ],
                style = {'textAlign':'center', 'marginTop':40,'marginBottom':40}),
         # Plot
@@ -100,7 +100,6 @@ def setup(name, N, itmax, r, dR):
         # Plot
            dcc.Graph(id = 'horizon_plot', figure = horizon_plot())
     ]
-    
     
     ## SETUP DASH
     app.layout = html.Div(id = 'parent', style = {'background-color': 'Beige'}, children = children)

Program/EFE_TR-Copy1.py
@@ -19,20 +19,18 @@ def ev_B(f0, r, dR, N, OPL):
 def ev_DA(f0, r, dR, N, OPL):
     al = f0[6*N:7*N]
     KA = f0[4*N:5*N]
-    Dal =f0[7*N:8*N]
     
-    p1 = KA[2:] * Dal[2:]
-    p2 = d_r(KA, dR, True)[2:]
+    p1 = KA[2:] * d_r(np.log(al[2:]), dR)
+    p2 = ghost_derivative(KA, dR)[2:]
     return - 2 * al[2:] * (p1 + p2)
 
 @njit
 def ev_DB(f0, r, dR, N, OPL):
     al = f0[6*N:7*N]
     KB = f0[5*N:6*N]
-    Dal =f0[7*N:8*N]
     
-    p1 = KB[2:] * Dal[2:]
-    p2 = d_r(KB, dR, True)[2:]
+    p1 = KB[2:] * d_r(np.log(al[2:]), dR)
+    p2 = ghost_derivative(KB, dR)[2:]
     return - 2 * al[2:] * (p1 + p2)
 
 
@@ -44,15 +42,15 @@ def ev_KA(f0, r, dR, N, OPL):
     KA = f0[4*N:5*N]
     KB = f0[5*N:6*N]
     al = f0[6*N:7*N]
-    Dal =f0[7*N:8*N]
     
     psi = 1 + 1 / 4 / r
+    Dal = d_r(np.log(al[2:]), dR)
     
-    p1 = d_r(Dal + DB, dR, True)[2:]
-    p2 = Dal[2:]**2 + 0.5 * (- Dal[2:] * DA[2:] + DB[2:]**2 - DA[2:] * DB[2:])
+    p1 = d_r(Dal, dR) + ghost_derivative(DB, dR)[2:]
+    p2 = Dal**2 + 0.5 * (- Dal * DA[2:] + DB[2:]**2 - DA[2:] * DB[2:])
     p3 = - psi[2:]**4 * A[2:] * KA[2:] * (KA[2:] + 2*KB[2:])
     p4 = - (DA[2:] - 2 * DB[2:]) / r[2:]
-    p5 = 4 * d2_r(np.log(psi[2:]), dR) + d_r(np.log(psi[2:]), dR) * (2 * DB[2:] - 2 * DA[2:] - 2 * Dal[2:] + 4 / r[2:])
+    p5 = 4 * d2_r(np.log(psi[2:]), dR) + d_r(np.log(psi[2:]), dR) * (2 * DB[2:] - 2 * DA[2:] - 2 * Dal + 4 / r[2:])
     
     return - al[2:] * (p1 + p2 + p3 + p4 + p5) / (A[2:] * psi[2:]**4)
 
@@ -65,16 +63,16 @@ def ev_KB(f0, r, dR, N, OPL):
     KA = f0[4*N:5*N]
     KB = f0[5*N:6*N]
     al = f0[6*N:7*N]
-    Dal =f0[7*N:8*N]
     
     psi = 1 + 1 / 4 / r
+    Dal = d_r(np.log(al[2:]), dR)
 	
-    p1 = d_r(DB, dR, True)[2:]
-    p2 = Dal[2:] * DB[2:] + DB[2:]**2 - 0.5 * DA[2:] * DB[2:]
-    p3 = - (DA[2:] - 2 * Dal[2:] - 4 * DB[2:]) / r[2:]
+    p1 = ghost_derivative(DB, dR)[2:]
+    p2 = Dal * DB[2:] + DB[2:]**2 - 0.5 * DA[2:] * DB[2:]
+    p3 = - (DA[2:] - 2 * Dal - 4 * DB[2:]) / r[2:]
     p4 = - 2 * (A[2:] - B[2:]) / (B[2:] * r[2:]**2)
     
-    p5 = 4 * d2_r(np.log(psi[2:]), dR) + d_r(np.log(psi[2:]), dR) * (8*d_r(np.log(psi[2:]), dR) + 4*Dal[2:] + 6*DB[2:] -2*DA[2:] + 12 / r[2:])
+    p5 = 4 * d2_r(np.log(psi[2:]), dR) + d_r(np.log(psi[2:]), dR) * (8*d_r(np.log(psi[2:]), dR) + 4*Dal + 6*DB[2:] -2*DA[2:] + 12 / r[2:])
 	
     p6 = al[2:] * KB[2:] * (KA[2:] + 2 * KB[2:])
     return - 0.5 * al[2:] * (p1 + p2 + p3 + p4 + p5) / (A[2:] * psi[2:]**4) + p6
@@ -92,35 +90,29 @@ def ev_al(f0, r, dR, N, OPL):
 
 
 @njit
-def ev_Dal(f0, r, dR, N, OPL):
-    if OPL:
-        A  = f0[0  :  N]
-        B  = f0[  N:2*N]
-        KA = f0[4*N:5*N]
-        KB = f0[5*N:6*N]
-        
-        psi = 1 + 1 / 4 / r
-        
-        return - 2 * d_r(KA[2:] / (A[2:] * psi[2:]**4) + 2 * KB[2:] / (r[2:]**2 * B[2:] * psi[2:]**4), dR)
-    else:
-        return np.zeros(N)[2:]
+def ev_Dal
 
-
+   
 @njit
-def d_r(f, dR, ghost = False):
+def ghost_derivative(f, dR):
     out = np.zeros_like(f)
-    if not ghost:
-        out[0:2] = (-25*f[0:2]/12+4*f[1:3]-3*f[2:4]+4*f[3:5]/3-f[4:6]/4)/dR
-    out[2:-2] = (f[0:-4]-8*f[1:-3]+8*f[3:-1]-f[4:])/(12*dR)
-    out[-2:] = (25*f[-2:]/12-4*f[-3:-1]+3*f[-4:-2]-4*f[-5:-3]/3+f[-6:-4]/4)/dR
+    out[2:-3] = ((f[0:-5] - f[4:-1]) / 12 - 2 * (f[1:-4] + f[3:-2]) / 3 )  /  dR
+    out[-3:-1] = (25 * f[-3:-1] / 12 - 4 * f[-4:-2] + 3 * f[-5:-3] - 4 * f[-6:-4] / 3 + f[-7:-5] / 4) / dR
     return out
-
+    
+@njit
+def d_r(f, dR):
+    out = np.zeros_like(f)
+    out[2:-3]  = ((f[0:-5] - f[4:-1]) / 12 - 2 * (f[1:-4] + f[3:-2]) / 3 )  /  dR
+    out[0:2]   = (- 25 * f[0:2] / 12 + 4 * f[1:3] - 3 * f[2:4] + 4 * f[3:5] / 3 - f[4:6] / 4) / dR
+    out[-3:-1] = (25 * f[-3:-1] / 12 - 4 * f[-4:-2] + 3 * f[-5:-3] - 4 * f[-6:-4] / 3 + f[-7:-5] / 4) / dR
+    return out
     
 @njit
 def d2_r(f, dR):
     out = np.zeros_like(f)
-    out[0:2]  = (35*f[0:2]-104*f[1:3]+114*f[2:4]-56*f[3:5]+11*f[4:6])/(12*dR**2)
-    out[2:-2] = (-f[0:-4]+16*f[1:-3]-30*f[2:-2]+16*f[3:-1]-f[4:])/(12*dR**2)
-    out[-2:]  = (11*f[-6:-4]-56*f[-5:-3]+114*f[-4:-2]-104*f[-3:-1]+35*f[-2:])/(12*dR**2)
+    out[2:-3]  = (-f[0:-5]+16*f[1:-4]-30*f[2:-3]+16*f[3:-2]-f[4:-1])/(12*dR**2)
+    out[0:2]   = (35*f[0:2]-104*f[1:3]+114*f[2:4]-56*f[3:5]+11*f[4:6])/(12*dR**2)
+    out[-3:-1] = (11*f[-7:-5]-56*f[-6:-4]+114*f[-5:-3]-104*f[-4:-2]+35*f[-3:-1])/(12*dR**2)
     return out
     

Program/Ex_1pLog.py
@@ -8,9 +8,9 @@ import argparse
 
 from FieldsClass import Fields
 from RK4 import rk4
-from EFE_TR import ghost_derivative
 from Horizon import comp_appHorizon
 from DashPlots import rundash
+from Constraint import comp_Hconstraint
 
 def progress_bar(current, total, t_med, bar_length=20):
     fraction = current / total
@@ -27,8 +27,10 @@ def main(N = 100, R = 2.5, dt = 0.0001, itmax = 100, OPL = 1, SAVE = 1, SILENT =
     if SAVE:
 	    name = f'N{N}_R{R}_t{itmax}.h5'
 	    h5f = h5py.File(name, 'w')
-    fields = Fields(R = R, N = N)
 
+    fields = Fields(R = R, N = N)
+    horizons = np.zeros((itmax+1, 4))
+    
     if dt > fields.dR:
         print(f'Warning: {dt = } > dR = {fields.dR}')
     
@@ -39,15 +41,14 @@ def main(N = 100, R = 2.5, dt = 0.0001, itmax = 100, OPL = 1, SAVE = 1, SILENT =
         fields.IC_1plusLogSlicing()
     else:
         fields.IC_GeodesicSlicing()
-    
-    horizons = np.zeros((itmax+1, 4))
-    j = 1
+
+    j = 0
     t = 0
     t_it = 0
     if not SILENT:
         print('Performing MoL evolution:')
 
-    while j <= itmax:
+    while j < itmax:
         t1 = time()
         fields.fields = np.copy(rk4(fields, dt))
         t += dt
@@ -57,12 +58,15 @@ def main(N = 100, R = 2.5, dt = 0.0001, itmax = 100, OPL = 1, SAVE = 1, SILENT =
         horizons[j,2] = horizons[j,1] * (1 + 1/4/horizons[j,1])**2
         
         if SAVE:
-            reshaped_output = np.reshape(fields.fields, (7, fields.N))
+            reshaped_output = np.reshape(fields.fields, (fields.nfields, fields.N))
             reshaped_output = np.delete(reshaped_output, [0,1], 1)
             h5f.create_dataset(f'{j}', data=reshaped_output, compression = 9)
             
         t_it += time() - t1 
+        
         j += 1
+        
+        #print(comp_Hconstraint(fields))
         if not SILENT:
             progress_bar(j, itmax, t_it/j)
         
@@ -112,10 +116,10 @@ if __name__ == "__main__":
     ## This first run is needed to compile Numba
     if not params['silentvar']:
         print(f'Initializing the simulation.')
-    main(N = 10, itmax = 3, SAVE = 0, SILENT = 1)
-    
+    #main(N = 10, itmax = 3, SAVE = 0, SILENT = 1)
+
     main(*list(params.values()))
     
-    ## RUIN FOR CONVERGENCE
+    # RUIN FOR CONVERGENCE
     #for n in [1000, 2000, 4000, 8000]:
     #    main(N = n, R = 10)

Program/FieldsClass.py
@@ -2,9 +2,10 @@ import numpy as np
 
 class Fields():
     
-    def __init__(self, R = 2.5, N = 100):
+    def __init__(self, R = 2.5, N = 100, nfields = 8):
         # Domain is a sphere of radius R
         self.OPL = False
+        self.nfields = nfields
         
         self.Rmax = R
         self.N    = N + 2 # 2 are the ghost cells at the left of each field
@@ -12,7 +13,7 @@ class Fields():
         self.dR  = R / N
     
         self.r = np.array([(j - 0.5)*self.dR for j in range(1,N+1)], dtype = np.float64)
-        self.fields = np.zeros(7 * self.N)
+        self.fields = np.zeros(nfields * self.N)
         self.psi = 1 + 1 / 4 / self.r  #r_s = 1
         
         self.r = np.insert(self.r, 0, [None, None])
@@ -32,6 +33,9 @@ class Fields():
         return self.fields[5*self.N:6*self.N]
     def al(self):
         return self.fields[6*self.N:7*self.N]
+    def Dal(self):
+        return self.fields[7*self.N:8*self.N]
+    
     
     def IC_1plusLogSlicing(self):
         """

Program/RK4.py
@@ -8,11 +8,11 @@ def eulerStep(fields, dt, k, fac):
             0: fields.A,  1: fields.B,
             2: fields.DA, 3: fields.DB,
             4: fields.KA, 5: fields.KB,
-            6: fields.al
+            6: fields.al, 7: fields.Dal
                     }
     
-    results = np.zeros(7 * fields.N)
-    for f in range(7):
+    results = np.zeros(fields.nfields * fields.N)
+    for f in range(fields.nfields):
         init = f*fields.N
         fin  = (f+1)*fields.N
         
@@ -31,26 +31,26 @@ def eulerStep(fields, dt, k, fac):
         results[fin-1] = ite(fields.r[-1])
     return results
 
-def RHS(f0, r, dr, N, OPL):
+def RHS(f0, r, dr, N, nfields, OPL):
     func_dict = {
             0: ev_A,  1: ev_B,
             2: ev_DA, 3: ev_DB,
             4: ev_KA, 5: ev_KB,
-            6: ev_al
+            6: ev_al, 7: ev_Dal
             }
-    rhs = np.zeros(7 * N)
-    for f in range(7):
+    rhs = np.zeros(nfields * N)
+    for f in range(nfields):
         rhs[N * f + 2 : N * f + N] = func_dict[f](f0, r, dr, N, OPL)
     return rhs
 
 def rk4(fields, dt):
-    k1 = RHS(fields.fields, fields.r, fields.dR, fields.N, fields.OPL)
-    k2 = RHS(eulerStep(fields, dt, k1, 0.5), fields.r, fields.dR, fields.N, fields.OPL)
-    k3 = RHS(eulerStep(fields, dt, k2, 0.5), fields.r, fields.dR, fields.N, fields.OPL)
-    k4 = RHS(eulerStep(fields, dt, k3, 1  ), fields.r, fields.dR, fields.N, fields.OPL)
+    k1 = RHS(fields.fields, fields.r, fields.dR, fields.N, fields.nfields, fields.OPL)
+    k2 = RHS(eulerStep(fields, dt, k1, 0.5), fields.r, fields.dR, fields.N, fields.nfields, fields.OPL)
+    k3 = RHS(eulerStep(fields, dt, k2, 0.5), fields.r, fields.dR, fields.N, fields.nfields, fields.OPL)
+    k4 = RHS(eulerStep(fields, dt, k3, 1  ), fields.r, fields.dR, fields.N, fields.nfields, fields.OPL)
     out = fields.fields + (k1/6 + k2/3 + k3/3 + k4/6) * dt / 2
     
-    for f in range(7):
+    for f in range(fields.nfields):
          init = f*fields.N
          fin  = (f+1)*fields.N
         # (A)symmetry condition